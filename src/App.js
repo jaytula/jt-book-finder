5de10315aab36d58f41057d1cc4116589683f135
Jay T <jaytula@gmail.com>
2019-02-28 02:18:25 +0000

    Implemented most recent searches in autocomplete datalist

--- src/App.js
+++ src/App.js
@@ -13,15 +13,40 @@ class App extends React.Component {
     this.state = {
       volumes: [],
       loading: false,
+      searchOptions: [],
       error: null,
       lastError: null,
     };
 
     this.doSearch = this.doSearch.bind(this);
     this.clearError = this.clearError.bind(this);
+    this.getSearchOptions = this.getSearchOptions.bind(this);
+    this.addSearchOption = this.addSearchOption.bind(this);
   }
 
-  componentDidMount() {}
+  getSearchOptions() {
+    let searchOptions = localStorage.getItem('searchOptions');
+    try {
+      searchOptions = searchOptions ? JSON.parse(searchOptions) : [];
+      this.setState({searchOptions});
+      return searchOptions;
+    } catch (err) {
+      console.error(err);
+      return [];
+    }
+  }
+
+  addSearchOption(item) {
+    let options = this.getSearchOptions();
+    if (options.indexOf(item) !== -1) return;
+    options.unshift(item);
+    options = options.slice(0, 10);
+    localStorage.setItem('searchOptions', JSON.stringify(options));
+    this.getSearchOptions();
+  }
+  componentDidMount() {
+    this.getSearchOptions();
+  }
 
   clearError() {
     this.setState({error: null});
@@ -31,6 +56,10 @@ class App extends React.Component {
     event.preventDefault();
     let formData = new FormData(event.target);
     let q = formData.get('q');
+    this.addSearchOption(q);
+    let inputElem = document.getElementById('q');
+    inputElem.blur();
+    inputElem.select();
     try {
       this.setState({loading: true});
       let result = await axios.get('/lookup', {params: {q}});
@@ -47,13 +76,24 @@ class App extends React.Component {
     let volumeElems = this.state.volumes.map((vol, key) => (
       <BookCard key={key} data={vol} />
     ));
+
+    let searchOptions = this.state.searchOptions.map((e, key) => (
+      <option key={key} value={e} />
+    ));
     return (
       <div>
         <header>
           <h1>Book Finder</h1>
           <div id="searchbox">
             <form onSubmit={this.doSearch}>
-              <input id="q" type="text" name="q" placeholder="Search" />
+              <input
+                id="q"
+                type="text"
+                list="search-options"
+                name="q"
+                placeholder="Search"
+                autoComplete="off"
+              />
               <Button type="submit">Search</Button>
             </form>
           </div>
@@ -70,9 +110,18 @@ class App extends React.Component {
           className={`error-toast ${this.state.error ? 'd-block' : 'd-none'}`}>
           {this.state.error && this.state.error.message}
         </div>
+        <datalist id="search-options">{searchOptions}</datalist>
       </div>
     );
   }
 }
 
 export default hot(App);
+
+/*
+ *
+ * Need a function to get from localStorange and return an empty array or current array.  This needs to be called at componentDidMount and result used to set the state.  Also called after an add.
+ * Need a function to add to localStorage.  This needs called after a search.  The operation of add.  Check if exists with indexOf before adding.
+ *
+ *
+ */
